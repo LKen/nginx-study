764aec8dc5254a504f620885eee515ac77f342aa
liangchuqiang <liangchuq@gmail.com>
2020-04-09 14:07:25 +0800

    123

---
+++ html1/robotmgr/static/bmap_offline_3.0/map_load.js
@@ -0,0 +1,31 @@
+var bmapcfg = {
+  'imgext'      : '.jpg',   //瓦片图的后缀 ------ 根据需要修改，一般是 .png .jpg
+  'tiles_dir'   : '/robotservice/file/tiles',       //普通瓦片图的地址，为空默认在 offlinemap/tiles/ 目录
+  'tiles_hybrid': '',       //卫星瓦片图的地址，为空默认在 offlinemap/tiles_hybrid/ 目录
+  'tiles_self'  : ''        //自定义图层的地址，为空默认在 offlinemap/tiles_self/ 目录
+};
+
+//////////////////下面的保持不动///////////////////////////////////
+var scripts = document.getElementsByTagName("script");
+var JS__FILE__ = scripts[scripts.length - 1].getAttribute("src");  //获得当前js文件路径
+bmapcfg.home = JS__FILE__.substr(0, JS__FILE__.lastIndexOf("/")+1); //地图API主目录
+(function(){
+  window.BMap_loadScriptTime = (new Date).getTime();
+  var $body = document.getElementsByTagName("body")[0];
+  //加载地图API主文件
+  var script_1 = document.createElement('script');
+  $body.appendChild(script_1);
+  script_1.src = bmapcfg.home+'bmap_offline_api_v3.0_min.js';
+  // document.write('<script type="text/javascript" src="'+bmapcfg.home+'bmap_offline_api_v3.0_min.js"></script>');
+  //加载扩展函数
+  var script_2 = document.createElement('script');
+  $body.appendChild(script_2);
+  script_2.src = bmapcfg.home+'map_plus.js';
+  // document.write('<script type="text/javascript" src="'+bmapcfg.home+'map_plus.js"></script>');
+  //加载城市坐标
+  var script_3 = document.createElement('script');
+  $body.appendChild(script_3);
+  script_3.src = bmapcfg.home+'map_city.js';
+  // document.write('<script type="text/javascript" src="'+bmapcfg.home+'map_city.js"></script>');
+})();
+///////////////////////////////////////////////////////////////////
